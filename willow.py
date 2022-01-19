f2b06bc09b53628d2b7c65b0f2b4219af9cc7531
NiSevva <95383094+NiSevva@users.noreply.github.com>
2022-01-19 12:33:01 -0600

    Add files via upload

---
+++ willow.py
@@ -0,0 +1,18 @@
+#pulls dependancies into the script
+import requests
+import json
+
+#user input to query which package/vuln they want to look for
+query = input("Enter which vuln/package you are looking for: ")
+type = input("What package manager do you want to search by? [use 'any' by default for all results]: ")
+
+#calling out to the api and putting it as a variable of 'response'
+response = requests.get("https://security.snyk.io/api/listing?q=" + query +"&type=" + type + "&pageNumber=1")
+
+#formatting json objects to be more pleasing on the eye
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=3)
+    print(text)
+
+#prints results of our json    
+jprint(response.json())
